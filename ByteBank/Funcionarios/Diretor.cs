24873aecc8bff97b047f32d00138475cbfb0ebc9
Martin Obrecht <martin.obrecht@yahoo.com.br>
2020-12-15 16:56:15 -0300

    Novas classes criadas

--- ByteBank/Funcionarios/Diretor.cs
+++ ByteBank/Funcionarios/Diretor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ByteBank.Sistemas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ByteBank.Funcionarios
 {
-    public class Diretor : Funcionario //herança
+    public class Diretor : Autenticavel //herança
     {
         //o salario inicial do cardo diretor é de 5000, por isso já esta declarado como argumento.
         public Diretor(string cpf) : base(5000, cpf)
@@ -20,7 +21,7 @@ namespace ByteBank.Funcionarios
         }
         public override double GetBonificacao() // é um método herdado do método virtual, qe foi declarada na classe base.
         {
-            return Salario + base.GetBonificacao(); // usamos o GetBonificação da base, ou seja, do funcionário.
+            return Salario *= 0.50; // usamos o GetBonificação da base, ou seja, do funcionário.
         }
     }
 }
---
+++ ByteBank/Sistemas/SistemaInterno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{ 
+
+    public class SistemaInterno
+    {
+        public bool Logar(Autenticavel funcionario, string senha)
+        {
+            bool usuarioAutenticado = funcionario.Autenticar(senha);
+
+            if (usuarioAutenticado)
+            {
+                Console.WriteLine("Bem-vindo ao sistema!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Senha Incorreta");
+                return false;
+            }
+        }
+    }
+}
